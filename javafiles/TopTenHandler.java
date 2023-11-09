4d41f2d51546814fd2239bf701fd8a7dc0bea9b3
Alvaro <111465731+Alvarito2222@users.noreply.github.com>
2023-11-08 20:16:24 -0500

    Add files via upload

---
+++ javafiles/TopTenHandler.java
@@ -0,0 +1,85 @@
+package guipackage;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class TopTenHandler implements ActionListener
+{
+
+    Connection connection;
+    JTable table;
+    DefaultTableModel model;
+    JScrollPane scroller;
+    JFrame myFrameClass;
+
+    TopTenHandler(Connection connection, JTable table, DefaultTableModel model, JScrollPane scroller, JFrame myFrameClass) {
+        this.connection = connection;
+        this.table = table;
+        this.model = model;
+        this.scroller = scroller;
+        this.myFrameClass = myFrameClass;
+    }
+
+    public void actionPerformed(ActionEvent e) 
+    {
+            try {
+                String query = "SELECT title, COUNT(title) AS stream_count\r\n"
+                		+ "FROM streams NATURAL JOIN timestamp\r\n"
+                		+ "WHERE stream_datetime > NOW() - INTERVAL 24 HOUR\r\n"
+                		+ "GROUP BY title\r\n"
+                		+ "ORDER BY stream_count DESC\r\n"
+                		+ "LIMIT 5;";
+                PreparedStatement statement = connection.prepareStatement(query);
+
+                ResultSet resultSet = statement.executeQuery();
+                ResultSetMetaData metaData = resultSet.getMetaData();
+                int columnCount = metaData.getColumnCount();
+
+                // Clear existing data
+                model.setRowCount(0);
+
+                // Set column names
+                model.setColumnCount(0);
+                for (int i = 1; i <= columnCount; ++i) {
+                    model.addColumn(metaData.getColumnName(i));
+                }
+
+                // Add rows to the model
+                while (resultSet.next()) {
+                    Object[] row = new Object[columnCount];
+                    for (int i = 1; i <= columnCount; ++i) {
+                        row[i - 1] = resultSet.getObject(i);
+                    }
+                    model.addRow(row);
+                }
+
+                // Set the model on the table and update the scroll pane if needed
+                table.setModel(model);
+                table.setFillsViewportHeight(true);
+
+              
+                    myFrameClass.getContentPane().add(scroller, BorderLayout.CENTER);
+               
+                    scroller.setViewportView(table);
+                
+
+                myFrameClass.validate();
+                statement.close();
+            } 
+            catch (SQLException ex) 
+            {
+                JOptionPane.showMessageDialog(null, ex.getMessage(), "Query error!", JOptionPane.ERROR_MESSAGE);
+            }
+    }
+}
