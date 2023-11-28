b9310c52c408f8eedb87b9715ab01d7ecb9b7cf7
Alvaro <111465731+Alvarito2222@users.noreply.github.com>
2023-11-28 17:10:31 -0500

    Update ProjectGui.java

--- javafiles/ProjectGui.java
+++ javafiles/ProjectGui.java
@@ -1,177 +1,4 @@
 
-
-import java.awt.Color;
-import java.sql.*;
-import javax.swing.*;
-import javax.swing.border.CompoundBorder;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.JTableHeader;
-import javax.swing.table.TableCellRenderer;
-
-import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
-
-public class ProjectGui {
-
-	public static void main(String args[]) {
-
-		System.out.println("Starting application.");
-
-		new MyFrameClass();
-
-		System.out.println("Done.");
-
-	}
-}
-
-class MyFrameClass extends JFrame {
-
-	private JLabel idLabel;
-	private JLabel pwdLabel;
-	private JTextField idField;
-	private JPasswordField pwdField;
-	JMenuBar menuBar;
-	JMenu menu;
-	JMenu history;
-	JMenu topten;
-	JMenuItem m1, m2, m3, m4, m5, m6, h1;
-
-	String role;
-
-	String loggedInUserId;
-	private JButton loginButton;
-
-	private JPanel loginPanel;
-
-	JTable table;
-	JScrollPane scroller;
-	DefaultTableModel tableModel;
-
-	final String driver = "com.mysql.cj.jdbc.Driver";
-	final String url = "jdbc:mysql://data-people.csamxrafss3m.us-east-1.rds.amazonaws.com:3306/streamingservice?useLegacyDatetimeCode=false&serverTimezone=America/New_York";
-
-	Connection connection;
-
-	MyFrameClass() {
-		setLayout(new BorderLayout());
-
-		try {
-			Class.forName(driver);
-
-			connection = DriverManager.getConnection(url, "alvaro", "groupProject1");
-		} catch (ClassNotFoundException ex) {
-			JOptionPane.showMessageDialog(null, "Failed to load JDBC driver!");
-			System.exit(1);
-		} catch (SQLException ex) {
-			JOptionPane.showMessageDialog(null, "Access denied!");
-			return;
-		}
-
-		table = new JTable() {
-	        public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
-	            Component c = super.prepareRenderer(renderer, row, column);
-	            if (!isRowSelected(row)) {
-	                c.setBackground(row % 2 == 0 ? new Color(230, 230, 250) : new Color(245, 245, 255)); // Light row colors
-	                c.setForeground(Color.BLACK); // Text color
-	            } else {
-	                c.setBackground(new Color(173, 216, 230)); // Color when a row is selected
-	            }
-	            return c;
-	        }
-	    };
-	    
-	    JTableHeader tableHeader = table.getTableHeader();
-	    tableHeader.setBackground(new Color(173, 216, 230)); // Light header background
-	    tableHeader.setForeground(Color.BLACK); // Header text color
-	    
-		tableModel = new DefaultTableModel();
-
-		scroller = new JScrollPane(table);
-		
-		scroller.getViewport().setBackground(new Color(230, 230, 250)); // Light viewport background
-		
-		table.setGridColor(new Color(173, 216, 230)); // Light grid lines
-
-		JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-		buttonPanel.setBackground(new Color(230, 230, 250)); // Match the loginPanel background
-
-		loginButton = new JButton("Login");
-		LoginHandler lhandler = new LoginHandler();
-		loginButton.addActionListener(lhandler);
-		
-		
-		loginButton.setPreferredSize(new Dimension(80, 25)); // Adjust width and height as needed
-
-		// Add the login button to the buttonPanel
-		buttonPanel.add(loginButton);
-
-
-		loginPanel = new JPanel();
-		idLabel = new JLabel("User Id");
-		pwdLabel = new JLabel("Password");
-		idField = new JTextField(10);
-		pwdField = new JPasswordField(10);
-		idLabel.setForeground(Color.BLACK);
-		pwdLabel.setForeground(Color.BLACK);
-		WindowHandler window = new WindowHandler();
-		this.addWindowListener(window);
-
-		loginPanel.setBackground(new Color(230, 230, 250));
-
-		loginButton.setBackground(new Color(173, 216, 230));
-		loginButton.setForeground(Color.BLACK);
-
-		loginPanel.setLayout(new GridLayout(2, 2, 0, 5));
-		loginPanel.add(idLabel);
-		loginPanel.add(idField);
-		loginPanel.add(pwdLabel);
-		loginPanel.add(pwdField);
-
-		// Panel to hold both the loginPanel and loginButton
-		JPanel northPanel = new JPanel(new BorderLayout());
-		northPanel.add(loginPanel, BorderLayout.CENTER);
-		northPanel.add(buttonPanel, BorderLayout.SOUTH);
-
-		// Add the northPanel to the NORTH region of the BorderLayout
-		add(northPanel, BorderLayout.NORTH);
-
-		setupMainFrame();
-	}
-
-	void setupMainFrame() {
-
-		Toolkit tk;
-		Dimension d;
-
-		tk = Toolkit.getDefaultToolkit();
-		d = tk.getScreenSize();
-		setSize(d.width / 2, d.height / 2);
-		setLocation(d.width / 4, d.height / 4);
-
-		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-
-		setTitle("DataPeopleGroupProject");
-
-		setVisible(true);
-	}
-
-	// inner class for handling login
-	class LoginHandler implements ActionListener {
-
-		
-
-		public void actionPerformed(ActionEvent e) {
-			String id = idField.getText();
-			loggedInUserId = id;
-			char[] p = pwdField.getPassword();
-			String pwd = new String(p);
-
-			try {
-				role = checkCredentials(id, pwd);
-
-				if ("ADMIN".equals(role)) {
-					initializeAdminGUI();
 				} else if ("MEMBER".equals(role)) {
 					initializeMemberGUI();
 				} else {
@@ -219,6 +46,18 @@ class MyFrameClass extends JFrame {
 		// Initialize the GUI for the administrator
 		JOptionPane.showMessageDialog(MyFrameClass.this, "Logged in as Administrator", "Login Success",
 				JOptionPane.INFORMATION_MESSAGE);
+
+		Toolkit tk;
+		Dimension d;
+
+		tk = Toolkit.getDefaultToolkit();
+		d = tk.getScreenSize();
+		setSize(d.width / 3 , d.height / 3);
+		setLocation(d.width / 3, d.height / 3);
+
+
+		loginPanel.setVisible(false);
+		buttonPanel.setVisible(false);
 
 		menuBar = new JMenuBar();
 		menu = new JMenu("Streams");
@@ -269,6 +108,7 @@ class MyFrameClass extends JFrame {
         TopTenHandler tenHandler = new TopTenHandler(connection, table,tableModel, scroller, this);
         topTenItem.addActionListener(tenHandler);
 
+
 		menuBar.add(menu);
 		menuBar.add(history);
 		menuBar.add(topten);
@@ -283,6 +123,17 @@ class MyFrameClass extends JFrame {
 		// Initialize the GUI for the member
 		JOptionPane.showMessageDialog(MyFrameClass.this, "Logged in as Member", "Login Success",
 				JOptionPane.INFORMATION_MESSAGE);
+
+		Toolkit tk;
+		Dimension d;
+
+		tk = Toolkit.getDefaultToolkit();
+		d = tk.getScreenSize();
+		setSize(d.width / 3 , d.height / 3);
+		setLocation(d.width / 3, d.height / 3);
+
+		loginPanel.setVisible(false);
+		buttonPanel.setVisible(false);
 
 		menuBar = new JMenuBar();
 		menu = new JMenu("Search By");
@@ -356,3 +207,6 @@ class MyFrameClass extends JFrame {
 	}
 
 }
+
+
+
