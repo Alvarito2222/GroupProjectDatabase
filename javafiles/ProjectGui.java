6e07e1569d7bdd51951516e2bc08126b06f9ee0d
Alvaro <111465731+Alvarito2222@users.noreply.github.com>
2023-11-28 17:13:05 -0500

    Update ProjectGui.java

--- javafiles/ProjectGui.java
+++ javafiles/ProjectGui.java
@@ -1,4 +1,230 @@
 
+import java.awt.Color;
+import java.sql.*;
+import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class ProjectGui {
+
+	public static void main(String args[]) {
+
+		System.out.println("Starting application.");
+
+		new MyFrameClass();
+
+		System.out.println("Done.");
+
+	}
+}
+
+class MyFrameClass extends JFrame {
+
+	private JLabel idLabel;
+	private JLabel pwdLabel;
+	private JTextField idField;
+	private JPasswordField pwdField;
+	JMenuBar menuBar;
+	JMenu menu;
+	JMenu history;
+	JMenu topten;
+	JMenuItem m1, m2, m3, m4, m5, m6, h1;
+
+	String role;
+
+	String loggedInUserId;
+	private JButton loginButton;
+
+	private JPanel loginPanel;
+	JPanel buttonPanel;
+
+	JTable table;
+	JScrollPane scroller;
+	DefaultTableModel tableModel;
+
+	final String driver = "com.mysql.cj.jdbc.Driver";
+	final String url = "jdbc:mysql://data-people.csamxrafss3m.us-east-1.rds.amazonaws.com:3306/streamingservice?useLegacyDatetimeCode=false&serverTimezone=America/New_York";
+
+	Connection connection;
+
+	MyFrameClass() {
+		setLayout(new BoxLayout(getContentPane(),BoxLayout.PAGE_AXIS));
+
+		try {
+			Class.forName(driver);
+
+			connection = DriverManager.getConnection(url, "alvaro", "groupProject1");
+		} catch (ClassNotFoundException ex) {
+			JOptionPane.showMessageDialog(null, "Failed to load JDBC driver!");
+			System.exit(1);
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Access denied!");
+			return;
+		}
+
+		table = new JTable() {
+	        public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+	            Component c = super.prepareRenderer(renderer, row, column);
+	            if (!isRowSelected(row)) {
+	                c.setBackground(row % 2 == 0 ? new Color(230, 230, 250) : new Color(245, 245, 255)); // Light row colors
+	                c.setForeground(Color.BLACK); // Text color
+	            } else {
+	                c.setBackground(new Color(173, 216, 230)); // Color when a row is selected
+	            }
+	            return c;
+	        }
+	    };
+	    
+	    JTableHeader tableHeader = table.getTableHeader();
+	    tableHeader.setBackground(new Color(173, 216, 230)); // Light header background
+	    tableHeader.setForeground(Color.BLACK); // Header text color
+	    
+		tableModel = new DefaultTableModel();
+
+		scroller = new JScrollPane(table);
+		
+		scroller.getViewport().setBackground(new Color(230, 230, 250)); // Light viewport background
+		
+		table.setGridColor(new Color(173, 216, 230)); // Light grid lines
+
+		buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BoxLayout(buttonPanel,BoxLayout.PAGE_AXIS));
+		buttonPanel.setBackground(new Color(57, 100, 195)); // Match the loginPanel background
+		loginButton = new JButton("Login");
+		LoginHandler lhandler = new LoginHandler();
+		onHover mouseAdapter = new onHover();
+		loginButton.addActionListener(lhandler);
+		loginButton.addMouseListener(mouseAdapter);
+
+
+		
+		
+		loginButton.setPreferredSize(new Dimension(80, 25)); // Adjust width and height as needed
+
+		// Add the login button to the buttonPanel
+
+		loginButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		loginButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+
+		buttonPanel.add(add(Box.createRigidArea(new Dimension(5, 10))));
+		buttonPanel.add(loginButton);
+		buttonPanel.add(add(Box.createRigidArea(new Dimension(5, 10))));
+
+
+		loginPanel = new JPanel();
+		idLabel = new JLabel("  User Id");
+		pwdLabel = new JLabel("  Password");
+		idField = new JTextField(10);
+		pwdField = new JPasswordField(10);
+		idLabel.setForeground(Color.BLACK);
+		pwdLabel.setForeground(Color.BLACK);
+		WindowHandler window = new WindowHandler();
+		this.addWindowListener(window);
+
+		loginPanel.setBackground(new Color( 57, 100, 195));
+
+		loginButton.setBackground(new Color(57, 100, 195));
+		loginButton.setForeground(Color.BLACK);
+		loginButton.setForeground(Color.WHITE);
+		loginButton.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+
+
+
+		loginPanel.setLayout(new GridLayout(4, 1, 0, 5));
+
+		//STYLING
+
+		//setUndecorated(true);
+
+		loginPanel.setPreferredSize(new Dimension(100,100));
+
+		idLabel.setHorizontalAlignment(JLabel.LEFT);
+		idLabel.setForeground(Color.WHITE);
+		idLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		idField.setHorizontalAlignment(JTextField.CENTER);
+		idField.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		idField.setPreferredSize(new Dimension(20,20));
+		idField.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+		idField.setForeground(Color.WHITE);
+		idField.setBackground(new Color(69, 116, 222));
+
+		pwdLabel.setHorizontalAlignment(JLabel.LEFT);
+		pwdLabel.setForeground(Color.WHITE);
+		pwdLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		pwdField.setHorizontalAlignment(JTextField.CENTER);
+		pwdField.setFont(new Font("Tahoma", Font.BOLD, 20));
+		pwdField.setPreferredSize(new Dimension(20,20));
+		pwdField.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+		pwdField.setForeground(Color.WHITE);
+		pwdField.setBackground(new Color(69, 116, 222));
+
+		buttonPanel.add(add(Box.createRigidArea(new Dimension(100, 0))));
+		loginPanel.add(idLabel);
+		loginPanel.add(idField);
+		loginPanel.add(pwdLabel);
+		loginPanel.add(pwdField);
+
+		// Panel to hold both the loginPanel and loginButton
+		JPanel northPanel = new JPanel(new BorderLayout());
+
+		northPanel.add(loginPanel, BorderLayout.CENTER);
+		northPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+		// Add the northPanel to the NORTH region of the BorderLayout
+		add(northPanel, BorderLayout.NORTH);
+
+		setupMainFrame();
+	}
+
+	void setupMainFrame() {
+
+		Toolkit tk;
+		Dimension d;
+
+		tk = Toolkit.getDefaultToolkit();
+		d = tk.getScreenSize();
+		setSize(d.width / 8, d.height / 4);
+		setLocation(d.width / 3, d.height / 3);
+
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+		setTitle("DataPeopleGroupProject");
+
+		setVisible(true);
+	}
+
+	public class onHover extends MouseAdapter{
+
+		public void mouseEntered(MouseEvent e) {
+			loginButton.setForeground(new Color(69, 116, 222));
+		}
+		public void mouseExited(MouseEvent e) {
+			loginButton.setForeground(Color.WHITE);
+		}
+	}
+
+	// inner class for handling login
+	class LoginHandler implements ActionListener {
+
+		
+
+		public void actionPerformed(ActionEvent e) {
+			String id = idField.getText();
+			loggedInUserId = id;
+			char[] p = pwdField.getPassword();
+			String pwd = new String(p);
+
+			try {
+				role = checkCredentials(id, pwd);
+
+				if ("ADMIN".equals(role)) {
+					initializeAdminGUI();
 				} else if ("MEMBER".equals(role)) {
 					initializeMemberGUI();
 				} else {
@@ -207,6 +433,7 @@
 	}
 
 }
+
 
 
 
