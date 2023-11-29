d00a73d20e3acacdfa4286e1c9d305c1ec2acfd3
Alvaro <111465731+Alvarito2222@users.noreply.github.com>
2023-11-28 21:54:27 -0500

    Update StreamsMenuHandler.java

--- javafiles/StreamsMenuHandler.java
+++ javafiles/StreamsMenuHandler.java
@@ -1,5 +1,3 @@
-
-
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -34,50 +32,74 @@ public class StreamsMenuHandler implements ActionListener {
 
 	}
 
+	@Override
 	public void actionPerformed(ActionEvent e) {
+	    try {
+	        // Fetch movie titles
+	        String titleQuery = "SELECT DISTINCT title FROM streams";
+	        PreparedStatement titleStmt = connection.prepareStatement(titleQuery);
+	        ResultSet titleRs = titleStmt.executeQuery();
+	        JComboBox<String> comboBox = new JComboBox<>();
 
-		String selectedTitle = e.getActionCommand();
+	        while (titleRs.next()) {
+	            comboBox.addItem(titleRs.getString("title"));
+	        }
 
-		try {
+	        // Check if there are movie titles to display
+	        if (comboBox.getItemCount() == 0) {
+	            JOptionPane.showMessageDialog(myFrameClass, "No movies found!", "Information", JOptionPane.INFORMATION_MESSAGE);
+	            return;
+	        }
 
-			String query = "SELECT email,title FROM streams WHERE title LIKE ?";
-			PreparedStatement statement = connection.prepareStatement(query);
-			statement.setString(1, "%" + selectedTitle + "%");
+	        JOptionPane.showMessageDialog(myFrameClass, comboBox, "Select Movie", JOptionPane.QUESTION_MESSAGE);
+	        titleStmt.close();
 
-			ResultSet resultSet = statement.executeQuery();
-			ResultSetMetaData metaData = resultSet.getMetaData();
-			int columnCount = metaData.getColumnCount();
+	        // Get the selected movie title from the combo box
+	        String selectedTitle = (String) comboBox.getSelectedItem();
 
-			// Clear existing data
-			model.setRowCount(0);
+	        if (selectedTitle != null && !selectedTitle.isEmpty()) {
+	            // Query to get stream details based on the selected title
+	            String query = "SELECT email, title FROM streams WHERE title LIKE ?";
+	            PreparedStatement statement = connection.prepareStatement(query);
+	            statement.setString(1, "%" + selectedTitle + "%");
 
-			// Set column names
-			model.setColumnCount(0);
-			for (int i = 1; i <= columnCount; ++i) {
-				model.addColumn(metaData.getColumnName(i));
-			}
+	            ResultSet resultSet = statement.executeQuery();
+	            ResultSetMetaData metaData = resultSet.getMetaData();
+	            int columnCount = metaData.getColumnCount();
 
-			// Add rows to the model
-			while (resultSet.next()) {
-				Object[] row = new Object[columnCount];
-				for (int i = 1; i <= columnCount; ++i) {
-					row[i - 1] = resultSet.getObject(i);
-				}
-				model.addRow(row);
-			}
+	            // Clear existing data
+	            model.setRowCount(0);
 
-			// Set the model on the table and update the scroll pane if needed
-			table.setModel(model);
-			table.setFillsViewportHeight(true);
+	            // Set column names
+	            model.setColumnCount(0);
+	            for (int i = 1; i <= columnCount; ++i) {
+	                model.addColumn(metaData.getColumnName(i));
+	            }
 
-			myFrameClass.getContentPane().add(scroller, BorderLayout.CENTER);
+	            // Add rows to the model
+	            while (resultSet.next()) {
+	                Object[] row = new Object[columnCount];
+	                for (int i = 1; i <= columnCount; ++i) {
+	                    row[i - 1] = resultSet.getObject(i);
+	                }
+	                model.addRow(row);
+	            }
 
-			scroller.setViewportView(table);
+	            // Set the model on the table and update the scroll pane if needed
+	            table.setModel(model);
+	            table.setFillsViewportHeight(true);
 
-			myFrameClass.validate();
-			statement.close();
-		} catch (SQLException ex) {
-			JOptionPane.showMessageDialog(null, ex.getMessage(), "Query error!", JOptionPane.ERROR_MESSAGE);
-		}
+	            myFrameClass.getContentPane().add(scroller, BorderLayout.CENTER);
+	            scroller.setViewportView(table);
+
+	            myFrameClass.validate();
+	            statement.close();
+	        }
+
+	    } catch (SQLException ex) {
+	        ex.printStackTrace();
+	        JOptionPane.showMessageDialog(null, ex.getMessage(), "Database Error", JOptionPane.ERROR_MESSAGE);
+	    }
 	}
+
 }
