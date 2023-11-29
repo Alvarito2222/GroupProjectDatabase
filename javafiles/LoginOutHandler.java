d1a8f584c801566b6912b7fe0b147596a505d67e
Alvaro <111465731+Alvarito2222@users.noreply.github.com>
2023-11-29 18:15:19 -0500

    Add files via upload

---
+++ javafiles/LoginOutHandler.java
@@ -0,0 +1,32 @@
+package guipackage;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+
+public class LoginOutHandler implements ActionListener {
+    Connection connection;
+    JTable table;
+    DefaultTableModel model;
+    JScrollPane scroller;
+    JFrame myFrameClass;
+    String loggedInUserId;
+    LoginOutHandler(Connection connection, JTable table, DefaultTableModel model, JScrollPane scroller,
+                       JFrame myFrameClass, String loggedInUserId) {
+        this.connection = connection;
+        this.table = table;
+        this.model = model;
+        this.scroller = scroller;
+        this.myFrameClass = myFrameClass;
+        this.loggedInUserId = loggedInUserId;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //System.out.println("Login Out");
+        loggedInUserId = "";
+        new MyFrameClass();
+        myFrameClass.dispose();
+    }
+}
